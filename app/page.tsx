a4af89e3eb253ee22e7387f7678196890d4a5829
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-04-28 00:06:12 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,215 @@
+'use client'
+import styles from "./styles/home.module.css";
+import useMousePosition from "./useMousePosition";
+import { useEffect, useRef } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+const experience = [
+  {
+    title: "Software Engineer",
+    company: "Logyq Protocol",
+    website: "https://logyqprotocol.com",
+    location: "Remote",
+    date: "2022 - 2023",
+    description: "Delivered multiple products and core features. As the first tech hire at Logyq, I played a pivotal role in shaping the technology stack.",
+    tags: ["React", "Next.js", "TypeScript", "Flutter", "Liquid", "Node.js", "Shopify API", "Discord API", "Python", "Wagmi.js", "Google Cloud", "Blockchain", "Cardano Node"],
+  },
+  {
+    title: "Fucking Around",
+    company: "Freelancer",
+    website: "",
+    location: "Remote",
+    date: "2018 - 2021",
+    description: "I helped dozens of SMEs digitalize their business and establish their online presence by building funnels and ecommerce stores.",
+    tags: ["Shopify", "Liquid", "Google Tag Manager", "Wordpress","Google Analytics", "Facebook Pixel", "Google Ads", "Facebook Ads"],
+  },
+];
 
 export default function Home() {
+  const wrapper = useRef<HTMLDivElement>(null);
+  const mousePosition = useMousePosition();
+
+  useEffect(() => {
+    if (wrapper.current) wrapper.current.style.background = `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`
+  }, [mousePosition]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+    <div ref={wrapper} className={styles.wrapper}>
+      <header className={styles.header}>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <img style={{borderRadius: "100px"}}width="150" src="https://media.licdn.com/dms/image/D4E03AQE9XiGD1WbbxQ/profile-displayphoto-shrink_800_800/0/1700247681332?e=1719446400&v=beta&t=hTyS180XBZuEGo4UUs_D7XQVPgl9I7G6peCczIwuZog"></img>
+          <h1>Luigi Bardella Gerbi</h1>
+          <h2>Software Engineer</h2>
+          <p>I learn fast, I’m extremely curious and practice self-awareness daily.</p>
+          <nav>
+            <ul>
+              <li className={styles.active}><a  href="#about">About</a></li>
+              <li><a href="#experience">Experience</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#hackatons">Hackatons</a></li>
+              <li><a href="#silicon">Silicon Valley</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </nav>
         </div>
-      </div>
+        <div>
+        </div>
+        <div className={styles.social}></div>
+      </header>
+      <main className={styles.main}>
+        <section id="about" className={styles.about}>
+          <h2>About me</h2>
+          <p>Ever since I wrote my first line of code at the age of 12, I have been captured by the joy of building. During high school and my degree in Business & Management, I helped dozens of SMEs digitalize their business and establish their online presence by building Funnels and Ecommerce stores.</p>
+          <p>After rediscovering my passion for coding during the lockdown period, I started to work as a Software Engineer (and first tech hire) at Logyq Protocol, a web3 startup, where I contributed to build their products from scratch. </p>
+          <p>In the first six months of 2023, while working remote for Logyq, I had the opportunity to couch surf across 10 countries, exploring different cultures and tastes.</p>
+          <p>Currently, I am participating in a unique program, GENOAES, that blends an accelerator with a master's degree, offered jointly by the University of Genoa and Georgetown University.</p>
+        </section>
+        <section id="experience" className={styles.experience}>          
+        <h2>Experience</h2>
+          {experience.map((exp, index) => (
+            <a key={index} href={exp.website}>
+              <div className={styles.item}>
+                <div className={styles.date}>{exp.date}</div>
+                <div className={styles.content}>
+                  <h3>{exp.title} · {exp.company}</h3>
+                  <p>{exp.description}</p>
+                  <div className={styles.pillContainer}>
+                    {exp.tags.map((tag, index) => (
+                      <div key={index} className={styles.pill}>{tag}</div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </a>
+          ))}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        </section>
+        <section id="projects" className={styles.projects}>
+          <h2>Projects</h2>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          <a href=""><div className={styles.item}>
+          <div className={styles.thumbnail}>
+            <img alt="project thumbnail" width={100}src={"https://i.pinimg.com/originals/9b/35/bd/9b35bda37a361d888596694c3dd9aad2.jpg"}/ >
+            </div>
+            <div className={styles.content}>
+                  <h3>KNN</h3>
+                  <p>In this project, I implemented the K-Nearest Neighbors (KNN) algorithm from scratch using Python. Then, trained the model on a dataset containing Pokémon statistics to predict whether a Pokémon is legendary or not based on its attributes.</p>
+                  <div className={styles.pillContainer}>
+                      <div className={styles.pill}>KNN</div>
+                      <div className={styles.pill}>Machine Learning</div>
+                      <div className={styles.pill}>Python</div>
+                      <div className={styles.pill}>Scikit-learn</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                  </div>
+                </div>
+          </div>
+          
+                
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+          </a>
+        </section>
+        <section id="projects" className={styles.projects}>
+          <h2>Hackatons</h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+In the chaotic realm of hackathons, success often boils down to the art of piecing things together with a mix of Red Bull-fueled energy and sheer determination, transforming ideas into (barely) working solutions.</p>
+          <a href=""><div className={styles.item}>
+          <div className={styles.thumbnail}>
+            <img alt="project thumbnail" width={100}src={"https://brittanychiang.com/_next/image?url=%2Fimages%2Fprojects%2Fhalcyon.png&w=640&q=75"}/ >
+            </div>
+            <div className={styles.content}>
+                  <h3>Talent Garden x Var Group (1st prize)</h3>
+                  <p>In just 36 hours, our team, consisting of me and Paolo Grignaffini, developed the idea and a working prototype to leverage Blockchain for building loyalty and trust in the private labels of one of the 12 partner companies. Using Next.js, Wagmi, and deploying our smart contracts on the Mumbai Testnet, we emerged as the first prize winners.</p>
+                  <div className={styles.linksContainer}>
+                    <span className={styles.link}><a href="https://www.linkedin.com/posts/vargroup-spa_il-team-di-lavoro-di-eurospin-vince-lhackathon-activity-6989176850501545984-LLT1/?utm_source=share&utm_medium=member_desktop">VarGroup: Congratulations to the Hackathon Winners!
+</a></span>
+                  </div>
+                  <div className={styles.pillContainer}>
+                      <div className={styles.pill}>Blockchain</div>
+                      <div className={styles.pill}>NFT</div>
+                      <div className={styles.pill}>Wagmi</div>
+                      <div className={styles.pill}>Next.js</div>
+                      <div className={styles.pill}>Solidity</div>
+
+                  </div>
+                </div>
+          </div>
+          
+
+          </a>
+          <a href=""><div className={styles.item}>
+          <div className={styles.thumbnail}>
+            <img alt="project thumbnail" width={100}src={"https://brittanychiang.com/_next/image?url=%2Fimages%2Fprojects%2Fhalcyon.png&w=640&q=75"}/ >
+            </div>
+            <div className={styles.content}>
+                  <h3>MasterZ (2nd prize)</h3>
+                  <p>Greengo, a blockchain-based crowdfunding platform structured as a Decentralized Autonomous Organization (DAO), aims to foster innovative and sustainable projects by connecting companies, creators, and users through a single ecosystem. It was selected among the top 10 projects and pitched to more than 300 people during the MasterZ Night live event, where it also achieved 2nd place.</p>
+                  <div className={styles.linksContainer}>
+                  </div>
+                  <div className={styles.pillContainer}>
+                      <div className={styles.pill}>Blockchain</div>
+                      <div className={styles.pill}>Wagmi</div>
+                      <div className={styles.pill}>Next.js</div>
+                      <div className={styles.pill}>Ether.js</div>
+                      <div className={styles.pill}>Hardhat</div>
+                      <div className={styles.pill}>Solidity</div>
+                  </div>
+                </div>
+          </div>
+          
+                
+
+          </a>
+          <a href=""><div className={styles.item}>
+          <div className={styles.thumbnail}>
+            <img alt="project thumbnail" width={100}src={"https://brittanychiang.com/_next/image?url=%2Fimages%2Fprojects%2Fhalcyon.png&w=640&q=75"}/ >
+            </div>
+            <div className={styles.content}>
+                  <h3>Encode x Algorand (2nd prize)</h3>
+                  <p>Building on its mission as a blockchain-based crowdfunding platform, Greengo secured second place among 12 finalists in the DeFi Challenge hackathon organized by Encode x Algorand. Following this achievement, it earned an invitation to join the Algorand x Encode Accelerator programme.</p>
+                  <div className={styles.linksContainer}>
+                    <span className={styles.link}><a href="">Encode x Algorand Hackathon: Prizewinners and Summary</a></span>
+                  </div>
+                  <div className={styles.pillContainer}>
+                      <div className={styles.pill}>Blockchain</div>
+                      <div className={styles.pill}>Wagmi</div>
+                      <div className={styles.pill}>Next.js</div>
+                      <div className={styles.pill}>Ether.js</div>
+                      <div className={styles.pill}>Hardhat</div>
+
+                  </div>
+                </div>
+          </div>
+          
+                
+
+          </a>
+          <a href=""><div className={styles.item}>
+          <div className={styles.thumbnail}>
+            <img alt="project thumbnail" width={100}src={"https://brittanychiang.com/_next/image?url=%2Fimages%2Fprojects%2Fhalcyon.png&w=640&q=75"}/ >
+            </div>
+            <div className={styles.content}>
+                  <h3>Venom Foundation</h3>
+                  <p>In just ten days, on behalf of Logyq Protocol, I embarked on a hackathon project, building a decentralized marketplace on the Venom blockchain. Juggling both smart contract development and frontend design.</p>
+                  <div className={styles.linksContainer}>
+                    <span className={styles.link}><a href="https://dorahacks.io/buidl/6057">Logyq Decentralized Marketplace: A Secure and Transparent Way to shop online
+</a></span>
+                  </div>
+                  <div className={styles.pillContainer}>
+                      <div className={styles.pill}>Blockchain</div>
+                      <div className={styles.pill}>Marketplace</div>
+                      <div className={styles.pill}>Wagmi</div>
+                      <div className={styles.pill}>Next.js</div>
+                      <div className={styles.pill}>Ether.js</div>
+                      <div className={styles.pill}>Hardhat</div>
+                      <div className={styles.pill}>Threaded Solidity</div>
+                  </div>
+                </div>
+          </div>
+          
+                
+
+          </a>
+        </section>
+      </main>
+    </div>
   );
 }
---
+++ app/useMousePosition.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useMousePosition = () => {
+  const [
+    mousePosition,
+    setMousePosition
+  ] = useState({ x: null, y: null });
+  useEffect(() => {
+    const updateMousePosition = (ev: { clientX: any; clientY: any; }) => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY });
+    };
+    window.addEventListener('mousemove', updateMousePosition);
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
+  return mousePosition;
+};
+export default useMousePosition;
